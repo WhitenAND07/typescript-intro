4ae94e64560f68632ba741d3eae439823bbc24d4
WhitenAND07 <jordi14and@gmail.com>
2022-05-19 13:14:46 +0200

    first commit

---
+++ src/exercises/01-tipos-basicos.ts
@@ -0,0 +1,19 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+//Declaration of constants
+const constNombreAdmin = 'Administrador';
+
+//Declaration of variables
+let nombre: string = "Jordi";
+let number: number = 100
+let isBoolean: boolean = true;
+
+//Declaration of variables of two types
+let stringOrNumber: string | number = "Jordi";
+stringOrNumber = 14;
+
+
+console.log(nombre, number, stringOrNumber);
+
