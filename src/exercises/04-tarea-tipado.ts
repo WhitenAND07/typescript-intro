62736b40361001510929c43ef1f85adf65c430e0
WhitenAND07 <jordi14and@gmail.com>
2022-05-20 09:51:18 +0200

    Exercise about interfaces

---
+++ src/exercises/04-tarea-tipado.ts
@@ -0,0 +1,35 @@
+import { ResolvedTypeReferenceDirectiveWithFailedLookupLocations } from "typescript";
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface Direccion{
+    calle: string;
+    pais: string;
+    ciudad: string;
+}
+
+interface SuperHero{
+    nombre: string;
+    edad: number;
+    direccion: Direccion;
+    mostrarDireccion: () => string;
+}
+
+
+const superHero : SuperHero = {
+    nombre: 'Spiderman',
+    edad: 30,
+    direccion: {
+        calle: 'Main St',
+        pais: 'USA',
+        ciudad: 'NY'
+    },
+    mostrarDireccion(){
+        return  this.nombre + ', ' + this.direccion.ciudad + ', ' + this.direccion.pais; 
+    }
+}
+
+const direccion: string = superHero.mostrarDireccion();
+console.log(direccion);
