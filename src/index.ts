09cd04337eba4f0b7f9737f002808e1929d359aa
WhitenAND07 <jordi14and@gmail.com>
2022-05-20 10:14:22 +0200

    Destructuring of Objects

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,35 @@
     ===== Código de TypeScript =====
 */
 
+interface Detalles{
+    autor: string;
+    anio: number;
+}
 
+interface Reproductor {
+    volumen: number;
+    segundo: number;
+    cancion: string;
+    detalles: Detalles
+}
+
+const reproductor: Reproductor = {
+    volumen: 90,
+    segundo: 40,
+    cancion: 'Hablando en plata',
+    detalles: {
+        autor: 'Melendi',
+        anio: 2003
+    }
+}
+
+const autor = 'Estopa';
+
+const {volumen, segundo, cancion, detalles } = reproductor;
+//AutorDetalle es la nomenclatura de la desestructuracion de el atributo autor
+const {autor: autorDetalle} = detalles;
+
+console.log('El volumen actual de: ', volumen);
+console.log('El segundo actual de: ', segundo);
+console.log('La cancion actual de: ', cancion);
+console.log('El autor es: ', autorDetalle);
